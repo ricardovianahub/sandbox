9e2383aab69b515076b40f11460c0f25b590c6ed
Ricardo Viana <ricardo@onlinebunker.com>
2020-11-25 17:47:20 -0600

    inter 1

---
+++ gof/src/test/java/behavioral/interpreter/Expr.java
@@ -0,0 +1,23 @@
+package behavioral.interpreter;
+
+import java.util.Map;
+
+public interface Expr {
+    int interpret(Map<String, Integer> context);
+
+    static Expr number(int number) {
+        return context -> number;
+    }
+
+    static Expr plus(Expr left, Expr right) {
+        return context -> left.interpret(context) + right.interpret(context);
+    }
+
+    static Expr minus(Expr left, Expr right) {
+        return context -> left.interpret(context) - right.interpret(context);
+    }
+
+    static Expr variable(String name) {
+        return context -> context.getOrDefault(name, 0);
+    }
+}
---
+++ gof/src/test/java/behavioral/interpreter/InterpreterTest.java
@@ -0,0 +1,57 @@
+package behavioral.interpreter;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import java.util.ArrayDeque;
+import java.util.Map;
+
+import org.junit.jupiter.api.Test;
+
+public class InterpreterTest {
+
+    private static Expr parseToken(String token, ArrayDeque<Expr> stack) {
+        Expr left, right;
+        switch(token) {
+            case "+":
+                right = stack.pop();
+                left = stack.pop();
+                return Expr.plus(left, right);
+            case "-":
+                right = stack.pop();
+                left = stack.pop();
+                return Expr.minus(left, right);
+            default:
+                return Expr.variable(token);
+        }
+    }
+    public static Expr parse(String expression) {
+        ArrayDeque<Expr> stack = new ArrayDeque<Expr>();
+        for (String token : expression.split(" ")) {
+            stack.push(parseToken(token, stack));
+        }
+        return stack.pop();
+    }
+
+//    @Test
+//    void interpret() {
+//        Expr expr = parse("w x z - +");
+//        Map<String, Integer> context = Map.of("w", 5, "x", 10, "z", 42);
+//        int result = expr.interpret(context);
+//        assertEquals(-27, result);
+//    }
+
+    private interface Lambda1 {
+        String execute(String text);
+    }
+
+    @Test
+    void lambdaExample() {
+        String prefix1 = "prefix1";
+        String separator = " - ";
+        Lambda1 lambda1 = text -> prefix1 + separator + text;
+
+        System.out.println(lambda1.execute("one"));
+        System.out.println(lambda1.execute("two"));
+        System.out.println(lambda1.execute("three"));
+    }
+}
