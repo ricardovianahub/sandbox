33fa9fb33eb1661ca39fe82c108d32baf78e639b
Ricardo Viana <ricardoviana@onlinebunker.com>
2021-02-01 18:03:09 -0600

    SB JDBC

---
+++ products/improvekata/ben/src/main/java/com/aa/improvekataben/ImprovekatabenApplication.java
@@ -0,0 +1,13 @@
+package com.aa.improvekataben;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ImprovekatabenApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ImprovekatabenApplication.class, args);
+	}
+
+}
---
+++ products/improvekata/ben/src/test/java/com/aa/improvekataben/ImprovekatabenApplicationTests.java
@@ -0,0 +1,13 @@
+package com.aa.improvekataben;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ImprovekatabenApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
