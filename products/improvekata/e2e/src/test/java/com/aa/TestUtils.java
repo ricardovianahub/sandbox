5c1ee6403e919d5d96e826a665621a71a1b970b3
Ricardo Viana <ricardo@onlinebunker.com>
2021-03-08 08:43:36 -0600

    Added error checks for docker compose not running

--- products/improvekata/e2e/src/test/java/com/aa/TestUtils.java
+++ products/improvekata/e2e/src/test/java/com/aa/TestUtils.java
@@ -9,6 +9,9 @@ public class TestUtils {
         try {
             Process process = run.exec("docker port reverse");
             String[] output = (new String(process.getInputStream().readAllBytes())).replaceAll("\n", "").split(":");
+            if (!output[0].startsWith("80/tcp")) {
+                throw new RuntimeException("ERROR ACCESSING REVERSE PROXY PORT - Is docker compose up and running? Does your user have access to the docker group?");
+            }
             return "http://localhost:" + output[output.length - 1];
         } catch (IOException ioe) {
             throw new RuntimeException(ioe);
