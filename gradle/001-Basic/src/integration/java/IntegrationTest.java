9f6cdbf7e888f89e11db86922c6e3296be876ec1
Ricardo Viana <ricardo@onlinebunker.com>
2021-10-15 13:34:10 -0500

    Gradle with integration test

---
+++ gradle/001-Basic/src/integration/java/IntegrationTest.java
@@ -0,0 +1,13 @@
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import org.junit.jupiter.api.Test;
+
+public class IntegrationTest {
+
+    @Test
+    void output() {
+        System.out.println("=== RUNNING IntegrationTest");
+        assertTrue(false);
+    }
+
+}
---
+++ gradle/001-Basic/src/test/java/UnitTest.java
@@ -0,0 +1,13 @@
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import org.junit.jupiter.api.Test;
+
+public class UnitTest {
+
+    @Test
+    void output() {
+        System.out.println("=== RUNNING UnitTest");
+        assertTrue(false);
+    }
+
+}
