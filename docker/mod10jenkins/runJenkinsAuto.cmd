edcbc4d7b9151832d13a8d903e1c47301a49025c
Ricardo Viana <ricardo@onlinebunker.com>
2021-10-20 14:45:47 -0500

    Next

--- docker/mod10jenkins/runJenkinsAuto.cmd
+++ docker/mod10jenkins/runJenkinsAuto.cmd
@@ -1,4 +1,4 @@
-#docker run --rm -v //var/run/docker.sock:/var/run/docker.sock alpine stat -c %%g /var/run/docker.sock > groupid.dat
-#set /p groupid=<groupid.dat
-#echo value is %groupid%
+rem docker run --rm -v //var/run/docker.sock:/var/run/docker.sock alpine stat -c %%g /var/run/docker.sock > groupid.dat
+rem set /p groupid=<groupid.dat
+rem echo value is %groupid%
 docker run --rm --name jenkinsauto -p:8098:8080 --group-add 0 -v //var/run/docker.sock:/var/run/docker.sock jenkinsauto
