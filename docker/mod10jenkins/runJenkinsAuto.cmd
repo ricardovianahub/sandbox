c1f118f0ce9aa30a0c3c60d44ffab55645dec179
Ricardo Viana <ricardo.viana@aa.com>
2021-10-20 12:50:04 -0500

    Jenkinns chown

--- docker/mod10jenkins/runJenkinsAuto.cmd
+++ docker/mod10jenkins/runJenkinsAuto.cmd
@@ -1,4 +1,4 @@
-docker run --rm -v //var/run/docker.sock:/var/run/docker.sock alpine stat -c %%g /var/run/docker.sock > groupid.dat
-set /p groupid=<groupid.dat
-echo value is %groupid%
-docker run --rm --name jenkinsauto -p:8098:8080 --group-add %groupid% -v //var/run/docker.sock:/var/run/docker.sock jenkinsauto
+#docker run --rm -v //var/run/docker.sock:/var/run/docker.sock alpine stat -c %%g /var/run/docker.sock > groupid.dat
+#set /p groupid=<groupid.dat
+#echo value is %groupid%
+docker run --rm --name jenkinsauto -p:8098:8080 --group-add 0 -v //var/run/docker.sock:/var/run/docker.sock jenkinsauto
