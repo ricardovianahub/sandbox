95aa02ceb77f634b6353adf9691b5a7712e71638
Ricardo Viana <ricardo.viana@aa.com>
2022-06-15 14:11:03 -0500

    multithread

---
+++ multithread/src/test/java/net/ricardoviana/multithread/MultithreadApplicationTests.java
@@ -0,0 +1,13 @@
+package net.ricardoviana.multithread;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MultithreadApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
